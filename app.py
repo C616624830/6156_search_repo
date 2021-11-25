4deb26df1e8b28a897d5bec615ff47a5b3ed1508
Lieyang Chen <chen616624830@gmail.com>
2021-11-25 12:23:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def before_request_func():
     if not result_ok:
         print('flag4')
         # print(url_for('/'))
-        return redirect('http://www.google.com')
+        return redirect(url_for('google.login'))
 
 
 
