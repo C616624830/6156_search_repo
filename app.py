1255d17ffc6d25f7a7391349305fcf9a141e81a0
Lieyang Chen <chen616624830@gmail.com>
2021-11-25 11:03:05 +0800

    test

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def before_request_func():
     if not result_ok:
         print('flag4')
         # print(url_for('/'))
-        return redirect(url_for('google.login'))
+        return redirect('http://3.133.105.233.nip.io:5000/login/google')
 
 
 
