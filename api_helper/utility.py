9dcc978709733eda526958b4d7b95eea4d2b6439
Lieyang Chen <chen616624830@gmail.com>
2021-12-10 16:07:34 +0800

    Update utility.py

--- api_helper/utility.py
+++ api_helper/utility.py
@@ -1,5 +1,5 @@
 from flask import Response
 import json
 
-def ret_message(status, message, headers = {}, code=2):
-    return Response(json.dumps({"message": message, "code": status, "headers": headers, "status": status}, default=str), content_type="application/json")
+def ret_message(status, message, headers = {}):
+    return Response(json.dumps({"status": status, "message": message, "headers": headers}, default=str), content_type="application/json")
